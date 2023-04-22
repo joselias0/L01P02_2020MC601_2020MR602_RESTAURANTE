1b2bf63f58963a539090d25ea648b797a102ed04
arturomanzurro <salvador.manzur@catolica.edu.sv>
2023-04-22 09:19:20 -0600

    Modelado de tablas

---
+++ L01P02_2020MC601_2020MR602/Models/clientes.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace L01P02_2020MC601_2020MR602.Models
+{
+    public class clientes
+    {
+        [Key]
+        public int clienteId { get; set; }
+        public string? nombreCliente { get; set; }
+        public string? direccion { get; set; }
+    }
+}
---
+++ L01P02_2020MC601_2020MR602/Models/motoristas.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace L01P02_2020MC601_2020MR602.Models
+{
+    public class motoristas
+    {
+        [Key]
+        public int motoristaId { get; set; }
+        public string? nombreMotorista { get; set; }
+    }
+}
