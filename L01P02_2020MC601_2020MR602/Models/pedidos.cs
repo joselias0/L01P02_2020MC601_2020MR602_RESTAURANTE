7cca28ce9c38878cc2a76da19a22bc44f86e76ae
joselias0 <jose.morales3@catolica.edu.sv>
2023-04-22 09:08:01 -0600

    creacion de modelos y su contexto

---
+++ L01P02_2020MC601_2020MR602/Models/pedidos.cs
@@ -0,0 +1,6 @@
+﻿namespace L01P02_2020MC601_2020MR602.Models
+{
+    public class pedidos
+    {
+    }
+}
---
+++ L01P02_2020MC601_2020MR602/Models/platos.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L01P02_2020MC601_2020MR602.Models
+{
+    public class platos
+    {
+        [Key]
+        public int platoId { get; set; }
+        public string? nombrePlato {get; set;}
+        public decimal? precio {get; set;}
+    }
+}
